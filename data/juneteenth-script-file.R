51562b92b3961ac13312db5ca52f65d2de35d4ff
Alyssa Carr <acarr@nelson-atkins.org>
2022-12-13 10:03:57 -0600

    Added script to rmd file to fix knitting issue.

--- data/juneteenth-script-file.R
+++ data/juneteenth-script-file.R
@@ -1,9 +1,3 @@
-library(readxl)
-library(readr)
-library(janitor)
-library(tidyverse)
-library(dplyr)
-library(gt)
 
 juneteenth <- read_excel (path = "data-raw/juneteenthforr_excel.xlsx")
 
@@ -45,7 +39,7 @@ activities <- juneteenth %>%
                                        "none_of_above" = "None"))
 
 
-race_ethnicity <- juneteenth %>% 
+race_ethinicity <- juneteenth %>% 
   pivot_longer(cols = race_a_a_or_black:missing_prefer_not_to_respond,
                names_to = "race_ethnicity",
                values_to = "race_values") %>% 
@@ -59,9 +53,7 @@ race_ethnicity <- juneteenth %>%
                                  "native_hawaiian_or_p_i" = "Native Hawaiian",
                                  "poc" = "BIPOC",
                                  "indicated_other" = "Other",
-                                 "missing_prefer_not_to_respond" = "Prefer Not to Respond")) %>% 
-  na_if("No") %>% 
-  drop_na()
+                                 "missing_prefer_not_to_respond" = "Prefer Not to Respond"))
 
 
 group <- juneteenth %>% 
@@ -88,6 +80,90 @@ juneteenth_attendance <- juneteenth %>%
                                         "attend_juneteenth_events_in_17" = "Present",
                                         "juneteenth_future" = "Future")) 
 
+juneteenth <- juneteenth %>% 
+  select("fid":"felt_inspired_uplifted") %>% 
+  select(-"other_fest_passport_to_india") %>% 
+  select(-"other_fest_cny") %>% 
+  select(-"other_fest_amer_indian") %>% 
+  select(-"other_fest_deaf_culture") %>% 
+  select(-"other_fest_big_picnic") %>% 
+  select(-"other_fest_day_of_dead") %>% 
+  select(-"other") %>% 
+  select(-"other_open_ended") %>% 
+  select(-"did_watched_a_perform") %>% 
+  select(-"did_created_something") %>% 
+  select(-"did_shared_memory_or_story") %>% 
+  select(-"did_interact_w_visitors_not_in_group") %>% 
+  select(-"did_saw_demonstration") %>% 
+  select(-"did_attended_story_time") %>% 
+  select(-"did_game_activity") %>% 
+  select(-"did_imagined_other_times") %>% 
+  select(-"did_reflected_on_significance_of_juneteenth") %>% 
+  select(-"did_none_of_above") %>% 
+  select(-"race_a_a_or_black") %>% 
+  select(-"race_american_indian_or_alaskan_native") %>% 
+  select(-"race_asian") %>% 
+  select(-"race_white") %>% 
+  select(-"race_hispanic_or_latino") %>% 
+  select(-"race_native_hawaiian_or_p_i") %>% 
+  select(-"race_poc") %>% 
+  select(-"race_indicated_other") %>% 
+  select(-"missing_prefer_not_to_respond") %>% 
+  select(-"visit_group_alone") %>% 
+  select(-"visit_group_spouse_s_o") %>% 
+  select(-"visit_group_child_ren_under_18") %>% 
+  select(-"visit_group_other_fam") %>% 
+  select(-"visit_group_friend_s") %>% 
+  select(-"visit_group_out_of_town_guests") %>% 
+  select(-"juneteenth_past") %>% 
+  select(-"attend_juneteenth_events_in_17") %>% 
+  select(-"juneteenth_future")
+
+
+juneteenth <- juneteenth %>% 
+  select(-"groups_with_children_under_18") %>% 
+  select(-"groups_without_children_under_18")
+
+
+juneteenth <- left_join(juneteenth,
+                        festivals,
+                        by = "fid")
+
+juneteenth <- left_join(juneteenth,
+                        group,
+                        by = "fid")
+
+juneteenth <- left_join(juneteenth,
+                        juneteenth_attendance,
+                        by = "fid")
+
+juneteenth <- left_join(juneteenth,
+                        race_ethinicity,
+                        by = "fid")
+
+
+write_csv(juneteenth,
+          file = "data/juneteenth.csv")
+
+write_csv(activities,
+          file = "data/activities.csv")
+
+write_csv(festivals,
+          file = "data/festivals.csv")
+
+write_csv(group,
+          file = "data/group.csv")
+
+write_csv(race_ethinicity,
+          file = "data/race_ethnicity.csv")
+
+write_csv(juneteenth_attendance,
+          file = "data/juneteenth_attendance.csv")
+
+
+write_rds(juneteenth,
+          file = "data/juneteenth.rds")
+
 write_rds(activities,
           file = "data/activities.rds")
 
@@ -97,7 +173,7 @@ write_rds(festivals,
 write_rds(group,
           file = "data/group.rds")
 
-write_rds(race_ethnicity,
+write_rds(race_ethinicity,
           file = "data/race_ethnicity.rds")
 
 write_rds(juneteenth_attendance,
